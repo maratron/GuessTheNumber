93fe3ac6638fc7273d06ccd168ab18cc7d90fa86
Nico Hämäläinen <nico@sizeof.io>
2016-03-16 22:56:34 +0200

    Initial commit

--- GuessTheNumber/main.swift
+++ GuessTheNumber/main.swift
@@ -1,12 +1,66 @@
-//
-//  main.swift
-//  GuessTheNumber
-//
-//  Created by Nico Hämäläinen on 16/03/16.
-//  Copyright © 2016 sizeof.io. All rights reserved.
-//
-
 import Foundation
+import GameplayKit
 
-print("Hello, World!")
+func secretNumber() -> Int {
+  return GKRandomSource.sharedRandom().nextIntWithUpperBound(100) + 1;
+}
 
+func getUserInput() -> String {
+  let data = NSFileHandle.fileHandleWithStandardInput().availableData
+  
+  guard let string = String(data: data, encoding: NSUTF8StringEncoding) else {
+    print("Could not get string from data.")
+    return ""
+  }
+  
+  let characterSet = NSCharacterSet.whitespaceAndNewlineCharacterSet()
+  return string.stringByTrimmingCharactersInSet(characterSet)
+}
+
+func getUserInputInt() -> Int {
+  guard let intValue = Int(getUserInput()) else {
+    return -1
+  }
+  return intValue
+}
+
+func startGame() {
+  let maximumTries: Int = 10
+  var currentTries: Int = 0
+  
+  let correctNumber: Int = secretNumber()
+  var guessedNumber: Int = -1
+  
+  print("Guess The Number! You have \(maximumTries) tries.")
+  while correctNumber != guessedNumber {
+    if currentTries == maximumTries {
+      print("Sorry, you ran out of attempts!")
+      print("Correct answer was \(correctNumber)")
+      break
+    }
+
+    print("Enter your guess:")
+    let guess = getUserInputInt()
+    guard guess != -1 else {
+      continue
+    }
+    guessedNumber = guess
+    
+    currentTries += 1
+    
+    if guessedNumber < correctNumber {
+      print("Too Low!")
+    }
+    else if guessedNumber > correctNumber {
+      print("Too High!")
+    }
+    else {
+      print("Correct!")
+      print("Number of tries: \(currentTries)")
+    }
+  }
+  
+  print("Thanks for playing!")
+}
+
+startGame()
