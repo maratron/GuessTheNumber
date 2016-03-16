954e74d95204be400b4840ec88b5e44ad32d5b45
Nico Hämäläinen <nico@sizeof.io>
2016-03-16 21:15:26 +0200

    Initial Commit

---
+++ GuessTheNumber/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GuessTheNumber
+//
+//  Created by Nico Hämäläinen on 16/03/16.
+//  Copyright © 2016 sizeof.io. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
